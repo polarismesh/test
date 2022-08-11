c627404991b538859a9eb440fc51ded15632bf26
atomzhong <atomzhong@tencent.com>
2022-08-11 19:31:32 +0800

    新增 grpc 测试 namespace 创建

--- src/polaris_test_case/01_polaris_namespace/01_polaris_namespace_base/polaris_namespace_create_except_console/namespace_create_from_grpc_api_check.py
+++ src/polaris_test_case/01_polaris_namespace/01_polaris_namespace_base/polaris_namespace_create_except_console/namespace_create_from_grpc_api_check.py
@@ -30,6 +30,7 @@ class NamespaceCreateFromGRPCApiCheck(PolarisTestCase):
         self.service_name = "AutoTestPolarisGRPCService--" + _random_str
 
         # ===========================
+        self.start_step("Get directory.")
         test_now_dir = os.path.abspath(__file__)
         self.log_info("Polaris-test now directory: " + test_now_dir)
         test_root_dir = os.path.abspath(os.path.join(test_now_dir, "../../../../.."))
@@ -37,15 +38,22 @@ class NamespaceCreateFromGRPCApiCheck(PolarisTestCase):
         test_resource_dir = test_root_dir + "/polaris_test_resource/polaris-go-demo"
         self.log_info("Polaris-test resource directory: " + test_resource_dir)
 
+        # ===========================
+        self.start_step("Create temp test directory.")
         case_name = type(self).__name__.lower()
         new_directory = "%s/temp-test/%s-%s" % (test_root_dir, case_name, _random_str)
         cmd_pre_deal_1 = "mkdir -p %s" % new_directory
         if os.system(cmd_pre_deal_1) != 0:
-            raise RuntimeError("Exec cmd %s error!" % cmd_pre_deal_1)
+            raise RuntimeError("Exec cmd: %s error!" % cmd_pre_deal_1)
+        else:
+            self.log_info("Exec cmd: %s success!" % cmd_pre_deal_1)
 
-        cmd_pre_deal_2 = "cp -r %s/* /root/%s" % (test_resource_dir, new_directory)
+
+        cmd_pre_deal_2 = "cp -r %s/* %s/" % (test_resource_dir, new_directory)
         if os.system(cmd_pre_deal_2) != 0:
-            raise RuntimeError("Exec cmd %s error!" % cmd_pre_deal_2)
+            raise RuntimeError("Exec cmd: %s error!" % cmd_pre_deal_2)
+        else:
+            self.log_info("Exec cmd: %s success!" % cmd_pre_deal_2)
 
         reg_ip = settings.POLARIS_SERVER_GRPC_SERVICE_ADDR
         host = socket.gethostbyname(socket.gethostname())
